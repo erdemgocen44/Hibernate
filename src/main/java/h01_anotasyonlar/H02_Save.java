68aa68c528a9c05a28f419f92b338392fca6662e
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-10-20 22:22:00 +0200

    ilk tablo

--- src/main/java/h01_anotasyonlar/H02_Save.java
+++ src/main/java/h01_anotasyonlar/H02_Save.java
@@ -1,16 +1,38 @@
 package h01_anotasyonlar;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class H02_Save {
     public static void main(String[] args) {
 
-        // BYRADAKİ IMPORTLAR HİBERNATE DEN YAPILIR
-        Configuration con = new Configuration().configure("hibernate.cfg.xml");
+        // ! BURADAKİ IMPORTLAR HİBERNATE DEN YAPILIR
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(H01_Sehir.class);
+        // ! SQL verilerinin olduğu cfg.xml classına bağlandık bu sayede sql e bağlandık
+        // ! ve h01_sehir classında işlem yapacağımızı belirttik
+
+        SessionFactory sf = con.buildSessionFactory();// ! işlem yapacağımız alanlar(Session) için parent alanlar
+                                                      // ! oluşturduk
+        Session s = sf.openSession();// !Parent alandan kendime küçük alanlar açtım
+
+        // Acilan session icerisinde islemlere baslayabilmek icin Transaction aciyoruz.
+        /*
+         * Transactionlar ile bir işlem yarıda kaldıysa veya tam olarak tamamlanadıysa
+         * tüm adımlar başa alınır veri ve işlem güvenliği için önemlidir.Kısacası ya
+         * hep ya hiç prensibine göre çalışır
+         */
+
+        Transaction tx = s.beginTransaction();
 
         H01_Sehir sehir1 = new H01_Sehir(34, "İstanbul");
         H01_Sehir sehir2 = new H01_Sehir(01, "Adana");
 
+        s.save(sehir1);
+        s.save(sehir2);
+
+        tx.commit();
     }
 
 }
